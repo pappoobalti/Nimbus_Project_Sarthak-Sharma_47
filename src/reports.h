ae0faf83e1070e09d4344bc4d1c33f5b2728b0e5
aaradhya-chauhan7 <aaradhyasc07@gmail.com>
2025-11-30 13:10:17 +0530

    Create reports.h

---
+++ src/reports.h
@@ -0,0 +1,9 @@
+#ifndef REPORTS_Module_H
+#define REPORTS_Module_H
+
+#include "loan.h"
+
+void showDefaulters(Loan list[], int count); // Shows borrowers who meet the criteria of being defaulters
+void showTotalAmount(Loan list[], int count); // Calculates and prints the total loan amount
+
+#endif
